37995b9bfbb892565354f0a77447828ce78140e3
Aryan Kashyap <matrixapp13@gmail.com>
2016-12-08 23:30:42 +0000

    Initial Commit

---
+++ OpenGLTutorial-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLTutorial-1
+//
+//  Created by Aryan Kashyap on 08/12/2016.
+//  Copyright © 2016 Aryan Kashyap. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
